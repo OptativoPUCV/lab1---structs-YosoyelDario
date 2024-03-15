b13e9e3d71e920654a6fbe32b9b5f27f02fac1c7
Your Name <you@example.com>
2024-03-15 13:39:09 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -98,12 +98,13 @@ y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
 int checkSorted(int arr[], int size)
 {
   int ordenA = 0, ordenD = 0;
-  for(int i  = 0; i < size; i++)
+  for(int i  = 0; i < size -1; i++)
   {
     if(arr[i] < arr[i + 1])
     {
       ordenA++;
     }
+    
     if(arr[i] > arr[i+1])
     {
       ordenD++;
